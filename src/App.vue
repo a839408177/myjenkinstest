dd60d70d51491b089d0c8b656d0aa0fb630cf5c9
a839408177 <50430734+a839408177@users.noreply.github.com>
2024-08-18 01:46:22 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div id="app">
+    itcaset
+
+    <HmButton></HmButton>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/button.vue
@@ -0,0 +1,32 @@
+<!--  -->
+<template>
+  <button class="hm-button">
+    <span>
+      <slot> </slot>
+    </span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "HmButton",
+  props: {
+    type: {
+      type: String,
+      default: "default",
+    },
+  },
+  data() {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+
+<style scoped>
+.hm-button {
+  color: red;
+}
+</style>
